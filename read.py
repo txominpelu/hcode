a5c56966a2eeee9486edf666ba6a12cfafac2982
Inigo Mediavilla <inigo.mediavilla@booking.com>
2017-02-23 19:27:00 +0100

    parsing input

---
+++ read.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import sys
+import pprint
+
+#5 2 4 3 100
+#50 50 80 30 110
+#1000 3
+#0 100
+#2 200
+#1 300
+#500 0
+#3 0 1500
+#0 1 1000
+#4 0 500
+#1 0 1000
+
+def read_header(line):
+    params = line.split(" ")
+    return {
+        "num_videos" : int(params[0]),
+        "num_endpoints" : int(params[1]),
+        "num_request_descriptions" : int(params[2]),
+        "num_caches" : int(params[3]),
+        "caches_size" : int(params[4].strip())
+    }
+
+def read_video_sizes(line):
+    return [int(x.strip()) for x in line.split(" ")]
+
+def read_endpoint(f):
+    [data_center_latency, num_connections] = f.next().split(" ")
+    num_connections = int(num_connections.strip())
+    data_center_latency = int(data_center_latency)
+    connections = []
+    for i in range(0, num_connections):
+        [endpoint, latency] = f.next().split(" ")
+        latency = int(latency.strip())
+        endpoint = int(endpoint.strip())
+        connections.append({"cache": endpoint, "latency": latency})
+    return { "data_center_latency" : data_center_latency,
+             "num_connections" : num_connections,
+             "connections" : connections}
+
+
+def read_request(f):
+    params = [ int(s.strip()) for s in f.next().split() ]
+    return { "video": params[0],
+      "endpoint": params[1],
+      "requests": params[2]}
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        params = read_header(f.next())
+        params["videos"] = read_video_sizes(f.next())
+        params["endpoints"] = [read_endpoint(f) for i in range(0,params['num_endpoints'])]
+        params["requests"] = [read_request(f) for i in range(0,params['num_request_descriptions'])]
+        pprint.pprint(params)
+
+
+
